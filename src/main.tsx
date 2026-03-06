2c1f2f3a23f7c5c2d96b2c6d0f8892fbfea76c05
Pavel Alekseev <alekseevpo@gmail.com>
2026-03-06 23:56:07 +0100

    chore: enable Vercel Speed Insights for real user metrics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from './App.tsx';
 import './index.css';
 
@@ -12,6 +13,7 @@ root.render(
         <HelmetProvider>
             <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
                 <App />
+                <SpeedInsights />
             </BrowserRouter>
         </HelmetProvider>
     </React.StrictMode>
