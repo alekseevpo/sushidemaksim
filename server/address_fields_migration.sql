f24d7de7ea90077d82c12d7b38481e9d36846793
Pavel Alekseev <alekseevpo@gmail.com>
2026-03-07 10:33:57 +0100

    chore: remove unused migration scripts and temporary files

--- server/address_fields_migration.sql
+++
@@ -1,3 +0,0 @@
--- Add house and apartment columns to user_addresses
-ALTER TABLE user_addresses ADD COLUMN IF NOT EXISTS house TEXT;
-ALTER TABLE user_addresses ADD COLUMN IF NOT EXISTS apartment TEXT;
