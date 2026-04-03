6cbc19ad3151cf2288630977485f39d227085394
Pavel Alekseev <alekseevpo@gmail.com>
2026-04-03 20:21:53 +0200

    feat: expand sitemap with menu categories and image SEO

--- server/src/routes/sitemap.ts
+++ server/src/routes/sitemap.ts
@@ -6,10 +6,21 @@ const router = Router();
 
 const STATIC_ROUTES = [
     { url: '', changefreq: 'daily', priority: '1.0' },
-    { url: '/menu', changefreq: 'weekly', priority: '0.8' },
+    { url: '/menu', changefreq: 'weekly', priority: '0.9' },
     { url: '/promo', changefreq: 'weekly', priority: '0.8' },
-    { url: '/contacts', changefreq: 'weekly', priority: '0.8' },
+    { url: '/contacts', changefreq: 'weekly', priority: '0.7' },
     { url: '/blog', changefreq: 'weekly', priority: '0.8' },
+];
+
+const MENU_CATEGORIES = [
+    'entrantes',
+    'rollos-grandes',
+    'rollos-clasicos',
+    'rollos-fritos',
+    'sopas',
+    'menus',
+    'extras',
+    'postre',
 ];
 
 router.get('/', async (req: Request, res: Response) => {
@@ -27,28 +38,52 @@ router.get('/', async (req: Request, res: Response) => {
     </url>`
         ).join('\n');
 
-        // 2. Dynamic Blog Posts
+        // 2. Menu Categories
+        const categoryItems = MENU_CATEGORIES.map(
+            cat => `    <url>
+        <loc>${baseUrl}/menu?category=${cat}</loc>
+        <lastmod>${today}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.7</priority>
+    </url>`
+        ).join('\n');
+        xmlItems += '\n' + categoryItems;
+
+        // 3. Dynamic Blog Posts with Images
         const { data: posts } = await supabase
             .from('blog_posts')
-            .select('slug, updated_at')
+            .select('title, slug, image_url, updated_at')
             .eq('published', true);
 
         if (posts && posts.length > 0) {
             const blogItems = posts
-                .map(
-                    post => `    <url>
+                .map(post => {
+                    let imgXml = '';
+                    if (post.image_url) {
+                        const imgUrl = post.image_url.startsWith('http')
+                            ? post.image_url
+                            : `${baseUrl}${post.image_url.startsWith('/') ? '' : '/'}${post.image_url}`;
+                        imgXml = `
+        <image:image>
+            <image:loc>${imgUrl}</image:loc>
+            <image:title>${post.title.replace(/[<>&"']/g, '')}</image:title>
+        </image:image>`;
+                    }
+
+                    return `    <url>
         <loc>${baseUrl}/blog/${post.slug}</loc>
         <lastmod>${post.updated_at ? new Date(post.updated_at).toISOString().split('T')[0] : today}</lastmod>
         <changefreq>monthly</changefreq>
-        <priority>0.6</priority>
-    </url>`
-                )
+        <priority>0.6</priority>${imgXml}
+    </url>`;
+                })
                 .join('\n');
             xmlItems += '\n' + blogItems;
         }
 
         const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 ${xmlItems}
 </urlset>`;
 
