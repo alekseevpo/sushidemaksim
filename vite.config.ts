a8b19a1275060290546eb8dca7c7460129d72724
Pavel Alekseev <alekseevpo@gmail.com>
2026-04-21 11:40:17 +0200

    chore(pwa): switch to autoUpdate registration type

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         react(),
         VitePWA({
-            registerType: 'prompt',
+            registerType: 'autoUpdate',
             includeAssets: [
                 'favicon.ico',
                 'logo.svg',
